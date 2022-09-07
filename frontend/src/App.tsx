67ec4f8a27fcc6372298fb0c0a24d73f9536a4d7
luzardo.fonseca <luzardo.fonseca@hotmail.com>
2022-09-07 19:39:20 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
 
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
+      <Header></Header>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard></SalesCard>
+          </div>
+        </section>
+      </main>
+
+      
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/image_1.svg'
+
+import './style.css'
+
+function Header() {
+
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por
+
+                        <a href="htpps://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    </p>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
